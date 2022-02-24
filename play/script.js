03a10d184badfc89b9c051c6f9a6d009a8749b56
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2022-02-24 16:15:22 -0500

    Added a script that tracks player colour. Simpler ways are possible, but I learnt a bit of the ways to browse the DOM. GG, JS. Will later adapt to make it simpler and work for each colour to continue to repertoire recording

--- play/script.js
+++ play/script.js
@@ -1,10 +1,19 @@
 let colourChoiceButtons = document.querySelectorAll(".modal--choose-colour")
-colourChoiceButtons.forEach(i => {i.addEventListener("click", event=> colourChosen(event));console.log(i);return undefined;})
+colourChoiceButtons.forEach(i => {i.addEventListener("click", event=> colourChosen(event));})
 let playerColour;
 // Event functions
 
 function colourChosen(event)
 {
-  if(event.target.tagName.toLowerCase() === "p" || event.target.tagName.toLowerCase() === "span"){};
-  playerColour == event.target.first;
+  console.log(event.target.tagName);
+  if(event.target.tagName === "P"){
+    console.log(Array.from(Array.from(event.target.parentNode.childNodes).filter(i=>i.nodeType == 1).filter(i=>i.tagName == "SPAN")[0].classList).filter(i => i == "black" || i == "white")[0]);//works
+
+    playerColour = Array.from(Array.from(event.target.parentNode.childNodes).filter(i=>i.nodeType == 1).filter(i=>i.tagName == "SPAN").classList)//.filter(i => {i == "black" || i == "white"}); // do not filter classList or childNodes entities without first converting to array
+  } else if(event.target.tagName === "SPAN"){
+    playerColour = Array.from(event.target.classList)//.filter(i => {i == "black" || i == "white"});
+  } else{
+    playerColour = Array.from(Array.from(event.target.childNodes).filter(i=>i.nodeType == 1).filter(i=>i.tagName == "SPAN").classList)//.filter(i => {i == "black" || i == "white"});
+  }
+  console.log(playerColour);
 };
